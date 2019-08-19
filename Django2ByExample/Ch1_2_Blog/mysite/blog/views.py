6be33c07476ba8a660cfed021b84427e20088585
Peter Nielson <computergeekpete@gmail.com>
2019-08-19 18:03:24 -0500

    Added template view for showing posts with similar tags

--- Django2ByExample/Ch1_2_Blog/mysite/blog/views.py
+++ Django2ByExample/Ch1_2_Blog/mysite/blog/views.py
@@ -4,22 +4,29 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
-
+from taggit.models import Tag
 
 # Create your views here
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+#class PostListView(ListView):
+#    queryset = Post.published.all()
+#    context_object_name = 'posts'
+#    paginate_by = 3
+#    template_name = 'blog/post/list.html'
 
-'''    
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)   # 3 objects at a time
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+  
+
+    paginator = Paginator(object_list, 3) # 3 Objects/posts at a time
     page = request.GET.get('page')
-    
+
+ 
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -32,8 +39,9 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
-'''
+                   'posts': posts,
+                   'tag': tag})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
