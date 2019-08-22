12ac857bbbe1ca208cf2166d65916e5aed100acc
Peter Nielson <computergeekpete@gmail.com>
2019-08-21 20:56:44 -0500

    Added templates to edit profile

--- Django2ByExample/Ch4_SocialSite/bookmarks/account/forms.py
+++ Django2ByExample/Ch4_SocialSite/bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -27,3 +28,23 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
 
         return cd['password2']
+
+
+
+#############################
+## User/Profile Edit Forms ##
+#############################
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name',
+                  'last_name',
+                  'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth',
+                  'photo')
