738f5fb95ea6a2d90b1081daef49ef0aa05367f3
Peter Nielson <computergeekpete@gmail.com>
2019-08-26 16:06:52 -0500

    Fixed semantic error

--- Django2ByExample/Ch4_SocialSite/bookmarks/bookmarks/settings.py
+++ Django2ByExample/Ch4_SocialSite/bookmarks/bookmarks/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'images.app.ImagesConfig',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
