34c1c3fb51758eaf071c119791cb6bb71962b40d
Peter Nielson <computergeekpete@gmail.com>
2019-08-27 15:10:18 -0500

    Added pagination, ajax, etc

--- Django2ByExample/Ch4_SocialSite/bookmarks/images/views.py
+++ Django2ByExample/Ch4_SocialSite/bookmarks/images/views.py
@@ -3,11 +3,41 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(page)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images',
+                       'images': images })
+
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images })
 
 
 @login_required
